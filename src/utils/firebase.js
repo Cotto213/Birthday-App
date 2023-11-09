7dc838e72e8572128766a978eb2a59e461165086
Cotto213 <alfonsocotto5b12@gmail.com>
2023-11-08 18:55:42 -0600

    Birthday

---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-pzTVsoZNxCZjo4uy2DdzSOCWSk_i_2A",
+  authDomain: "birthday-73714.firebaseapp.com",
+  projectId: "birthday-73714",
+  storageBucket: "birthday-73714.appspot.com",
+  messagingSenderId: "443779467135",
+  appId: "1:443779467135:web:966f1b7e7c0b49db7dbc4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
